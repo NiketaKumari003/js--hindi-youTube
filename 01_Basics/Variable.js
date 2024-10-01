eac2f01c9606f30ede32b207907ab5134951c303
NIKETA KUMARI <116382623+NiketaKumari003@users.noreply.github.com>
2024-10-01 18:26:54 +0000

    third commit

---
+++ 01_Basics/Variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "niketa78709846@gmail.com"
+var accountPassword = "12345"
+accounttCity = "jaipur"
+accountEmail = "abcd@gmail.com"
+accountPassword="1234567"
+accountCity="Bengaluru"
+let accountState;
+
+
+//accountId=2// not allowed
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/* 
+Prefer not to use var
+because of issue in the block scope and functional scope
+*/
